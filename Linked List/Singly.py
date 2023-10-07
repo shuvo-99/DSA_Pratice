c1cc5c9df0a09fdcab5536e4475e5a369dfb6780
shuvo-99 <anontoshuvo99@gmail.com>
2023-10-07 11:09:48 +0600

    singly add-end

--- Linked List/Singly.py
+++ Linked List/Singly.py
@@ -25,8 +25,23 @@ class LinkedList:
     new_node.ref = self.head
     self.head = new_node
 
+  # add at the end
+  def addEnd(self, data):
+    new_node = Node(data)
+
+    # check Linked List empty or not
+    if self.head == None:
+      self.head = new_node
+
+    else:
+      n = self.head
+      while n.ref != None:
+        n = n.ref
+      n.ref = new_node
+
+
 ll1 = LinkedList()
 ll1.addBegin(10)
-ll1.printDteails() 
+ll1.addEnd(100)
 ll1.addBegin(20)
 ll1.printDteails()
