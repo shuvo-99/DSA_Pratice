e56db814496d4bbff991f77d44fcb4dad44ac300
shuvo-99 <anontoshuvo99@gmail.com>
2023-10-13 00:08:53 +0600

    new problem

---
+++ Array/Leet Code/26. Remove Duplicates From Sorted Array.py
@@ -0,0 +1,29 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        unique_list = []
+        
+        for i in nums:
+            if i not in unique_list:
+                unique_list.append(i)
+
+        nums.clear()
+        for i in unique_list:
+            nums.append(i)
+        
+        return len(nums)
+
+# Alternate Solution
+
+class Solution:
+    def removeDuplicates(self, nums):
+        
+        if len(nums)<2:
+            return len(nums)
+        
+        pre = 0
+        for cur in range(1,len(nums)):
+            if nums[cur]!=nums[pre]:
+                pre+=1
+                nums[pre]=nums[cur]
+
+        return pre+1
