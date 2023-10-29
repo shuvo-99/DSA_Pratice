eff60e7444beb519d62c6f7affad38470e968633
shuvo-99 <anontoshuvo99@gmail.com>
2023-10-29 13:19:08 +0600

    BST insert

--- BInary Search Tree/Binary Search Tree.py
+++ BInary Search Tree/Binary Search Tree.py
@@ -4,7 +4,34 @@ class BST:
     self.left_child = None
     self.right_child = None
 
+  def insertInBST(self, data):
+    if self.key == None:
+      self.key == data
+      return
+    
+    elif self.key == data:
+      print('Duplicate value are not added')
+      return
+
+    else:
+      if self.key > data:
+        if self.left_child == None:
+          self.left_child = BST(data)
+
+        else:
+          self.left_child.insertInBST(data)
+
+      else:
+        if self.right_child == None:
+          self.right_child = BST(data)
+
+        else:
+          self.right_child.insertInBST(data)
+
+    
+
 root = BST(10)
+root.insertInBST(20)
 print(root.key)
 print(root.left_child)
 print(root.right_child)
