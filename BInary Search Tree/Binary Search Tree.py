a0748a0dea16cdaa656aa9e119154cf1b725e4d7
shuvo-99 <anontoshuvo99@gmail.com>
2023-10-30 13:42:14 +0600

    new problem

--- BInary Search Tree/Binary Search Tree.py
+++ BInary Search Tree/Binary Search Tree.py
@@ -5,28 +5,29 @@ class BST:
     self.right_child = None
 
   def insertInBST(self, data):
-    if self.key == None:
-      self.key == data
+
+    if self.key == None:                       # Check if tree is empty or not
+      self.key == data                         # If empty, insert as first node
       return
     
-    elif self.key == data:
-      print('Duplicate value are not added')
+    elif self.key == data:                     # Check the value is duplicate
+      print('Duplicate value are not added')   # If duplicate, we ignore the value
       return
 
-    else:
-      if self.key > data:
-        if self.left_child == None:
-          self.left_child = BST(data)
+    else:                                      # Finding the position of the new_Node
+      if self.key > data:                      # Check the value is greater or less than the root value
+        if self.left_child == None:            # if less than root value, check root's left cild is empty or not
+          self.left_child = BST(data)          # if empty, create a new node and store its address in the left child of the root
 
         else:
-          self.left_child.insertInBST(data)
+          self.left_child.insertInBST(data)    # if root's left cild is not empty, then repeat the process and now new root will be the previous root's left cild
 
       else:
-        if self.right_child == None:
-          self.right_child = BST(data)
+        if self.right_child == None:           # if greater than root value, check root's right cild is empty or not 
+          self.right_child = BST(data)         # if empty, create a new node and store its address in the right child of the root
 
         else:
-          self.right_child.insertInBST(data)
+          self.right_child.insertInBST(data)   # if root's right cild is not empty, then repeat the process and now new root will be the previous root's right cild
 
     
 
