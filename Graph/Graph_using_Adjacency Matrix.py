3bc4a9abcc9cafa54bc83899b3839e1e59c64ba2
shuvo-99 <anontoshuvo99@gmail.com>
2023-11-16 21:15:37 +0600

    graph delete

--- Graph/Graph_using_Adjacency Matrix.py
+++ Graph/Graph_using_Adjacency Matrix.py
@@ -71,6 +71,29 @@ def delete_node(v):
     for i in graph:
       i.pop(index1)
 
+# For undirected
+def delete_edge(v1, v2):
+  if v1 not in nodes:
+    print(v1, 'is not present in the graph')
+  elif v2 not in nodes:
+    print(v1, 'is not present in the graph')
+  else:
+    index1 = nodes.index(v1)    
+    index2 = nodes.index(v2)
+    graph[index1][index2] = 0   
+    graph[index2][index1] = 0
+
+# For directed
+def delete_edge(v1, v2):
+  if v1 not in nodes:
+    print(v1, 'is not present in the graph')
+  elif v2 not in nodes:
+    print(v1, 'is not present in the graph')
+  else:
+    index1 = nodes.index(v1)    
+    index2 = nodes.index(v2)
+    graph[index1][index2] = 0
+
 def print_graph():
   for i in range(node_count):
     for j in range(node_count):
@@ -86,13 +109,15 @@ add_Node('A')
 add_Node('B')
 add_Node('C')
 
-add_edge('A','B')
-add_edge('A','C')
-add_edge('B','C')
+add_edge('A','B',5)
+add_edge('A','C',6)
+add_edge('B','C',7)
 
 add_Node('D')
 
 delete_node('A')
+delete_edge('A','B')
 
+print(nodes)
 print(graph)
 print_graph()
