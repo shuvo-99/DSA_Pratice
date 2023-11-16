af60f15fe06200b9c70fe6850717a349bde74931
shuvo-99 <anontoshuvo99@gmail.com>
2023-11-16 18:44:59 +0600

    graph insertion node + edge

--- Graph/Graph_using_Adjacency List.py
+++ Graph/Graph_using_Adjacency List.py
@@ -5,11 +5,22 @@ def add_Node(v):
   else:
     graph[v] = []
 
+# For undirected and unweighted
+def add_edge(v1, v2):
+  if v1 not in graph:
+    print(v1, 'is not present in the graph')
+  elif v2 not in graph:
+    print(v1, 'is not present in the graph')
+  else:
+    graph[v1].append(v2)
+    graph[v2].append(v1)
 
 graph = {}
 
 add_Node('A')
 add_Node('B')
 add_Node('C')
+
+add_edge('A','B')
 
 print(graph)
