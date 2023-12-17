042a64447a0f5e352a2ee448fba2ba6db0294fa6
shuvo-99 <anontoshuvo99@gmail.com>
2023-12-17 13:22:46 +0600

    new problem

--- Problems/Linked List/GeeksforGeeks/Segregate even and odd nodes in a Linked List.py
+++ Problems/Linked List/GeeksforGeeks/Segregate even and odd nodes in a Linked List.py
@@ -34,38 +34,63 @@ class LinekedList:
     print()
 
   def segregate(self):
-    dummy = Node(0)
 
-    tail = self.head
-    d_tail = dummy
+  # === Process - 1 ===
+    
+  #   dummy = Node(0)
+  #   tail = self.head
+  #   d_tail = dummy
 
-    while tail.next:
-      if tail.next.next:
-        if tail.next.data%2 == 0:
-          while d_tail.next:
-            d_tail = d_tail.next
-          d_tail.next = tail.next
-          tail.next = tail.next.next
-          d_tail.next.next = None 
-          tail = tail
-        else:  
-          tail = tail.next
+  #   while tail.next:
+  #     if tail.next.next:
+  #       if tail.next.data%2 == 0:
+  #         while d_tail.next:
+  #           d_tail = d_tail.next
+  #         d_tail.next = tail.next
+  #         tail.next = tail.next.next
+  #         d_tail.next.next = None 
+  #         tail = tail
+  #       else:  
+  #         tail = tail.next
       
-      else:
-        if tail.next.data%2 == 0:
-          while d_tail.next:
-            d_tail = d_tail.next
-          d_tail.next = tail.next
-          tail.next = None
-          tail = tail
-        else:  
-          tail = tail.next
+  #     else:
+  #       if tail.next.data%2 == 0:
+  #         while d_tail.next:
+  #           d_tail = d_tail.next
+  #         d_tail.next = tail.next
+  #         tail.next = None
+  #         tail = tail
+  #       else:  
+  #         tail = tail.next
 
     
-    while d_tail.next:
-      d_tail = d_tail.next
-    d_tail.next = self.head
-    self.head = dummy.next
+  #   while d_tail.next:
+  #     d_tail = d_tail.next
+  #   d_tail.next = self.head
+  #   self.head = dummy.next
+    
+  #  === Process -2 ===
+  
+    dummy_even = Node(0)
+    dummy_odd = Node(0)
+
+    tail = self.head
+    even_tail = dummy_even
+    odd_tail = dummy_odd
+
+    while tail:
+        if tail.data % 2 == 0:
+            even_tail.next = tail
+            even_tail = even_tail.next
+        else:
+            odd_tail.next = tail
+            odd_tail = odd_tail.next
+        tail = tail.next
+
+    even_tail.next = dummy_odd.next
+    odd_tail.next = None
+
+    self.head = dummy_even.next
 
 
 ll1 = LinekedList([17, 15,8,12,10, 5,4,1,7,6])
